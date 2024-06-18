298e315a5260b052930a749e6efebc2c71826272
Austin Taylor <86385162+Atay365@users.noreply.github.com>
2024-06-18 12:57:54 -0500

    Restructured the <ul> elements to aid in the user interface.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import './App.scss'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import "./App.scss";
+import WarehousePage from "./pages/WarehousePage/WarehousePage";
 
 function App() {
   const port = import.meta.env.VITE_PORT || "http://localhost:8080";
@@ -9,9 +10,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/warehouses" element={<WarehousePage />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,35 +1,55 @@
+import chevron from "../../assets/images/icons/nav/chevron_right-24px.svg";
+import trashcan from "../../assets/images/icons/action/delete_outline-24px.svg";
+import editicon from "../../assets/images/icons/action/edit-24px.svg";
+import "./WarehouseList.scss";
+
 const WarehouseList = () => {
   return (
-    <section>
-      <header>
-        <h1>Warehouse</h1>
-        <div>
-          <input type="search" name="warehouseSearch" />
-          <button>+ Add New Warehouse</button>
+    <section className="warehouses">
+      <header className="warehouses__header">
+        <h1 className="warehouses__title">Warehouses</h1>
+        <div className="warehouses__header-container">
+          <input
+            type="search"
+            name="warehouseSearch"
+            className="warehouses__search"
+            placeholder="Search..."
+          />
+          <button className="warehouses__add-button">
+            + Add New Warehouse
+          </button>
         </div>
       </header>
-      <ul>
-        <li>
-          Manhattan{" "}
-          <img
-            src="../../assets/images/icons/nav/chevron_right-24px.svg"
-            alt=""
-          />
-        </li>
-        <li>503 Broadway, New York, USA</li>
-        <li>Parmin Aujla</li>
-        <li>
-          <div>
-            <p>2057903759</p>
-            <p>atay1109@gmail.com</p>
+
+      {/* Had to rewrite entire thing. Was breaking my brain for a second */}
+      <ul className="warehouses__list">
+        <li className="warehouses__item">
+          <div className="warehouses__info-group">
+            <div className="warehouses__info-item">
+              <p className="warehouses__sub-title">WAREHOUSE</p>
+              <div className="link-arrow">
+                <p className="warehouses__info">Manhattan</p>
+                <img src={chevron} alt="Enter Arrow" />
+              </div>
+            </div>
+            <div className="warehouses__info-item">
+              <p className="warehouses__sub-title">ADDRESS</p>
+              <p className="warehouses__info">503 Broadway, New York, USA</p>
+            </div>
+            <div className="warehouses__info-item">
+              <p className="warehouses__sub-title">CONTACT NAME</p>
+              <p className="warehouses__info">Parmin Aujla</p>
+            </div>
+            <div className="warehouses__info-item">
+              <p className="warehouses__sub-title">CONTACT INFORMATION</p>
+              <p className="warehouses__info">+1 (629) 555-0129</p>
+              <p className="warehouses__info">paujla@instock.com</p>
+            </div>
+            <div className="warehouses__info-item warehouses__info-item--actions">
+              <img src={trashcan} alt="Delete" className="warehouses__icon" />
+              <img src={editicon} alt="Edit" className="warehouses__icon" />
+            </div>
           </div>
-        </li>
-        <li>
-          <img
-            src="../../assets/images/icons/action/delete_outline-24px.svg"
-            alt=""
-          />{" "}
-          <img src="../../assets/images/icons/action/edit-24px.svg" alt="" />
         </li>
       </ul>
     </section>
