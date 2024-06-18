520e3c88ef96d3d8793ac68b8b3d22670b90fa5c
Yongsheng <ygong56@uwo.ca>
2024-06-18 15:54:52 -0400

    A24ON-7-header-create a basic header for mobile only and also set up a route for Inventory component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
 import "./Header.scss"
-
-function Header() {
+import logo from "../../assets/images/logos/inStock-Logo.svg"
+import { Link } from "react-router-dom";
+function Header({ button }) {
     return (
-        <>
-        </>
+        <header className="header">
+            <img className="header__logo" src={logo}></img>
+            <nav className="header__nav">
+                <ul className="header__nav-list">
+                    <li className="header__nav-list-warehouses">
+                        <button className={button === "warehousesButton" ? "activeButton" : "inactiveButton"}>
+                            <Link to="/">Warehouses</Link>
+                        </button>
+                    </li>
+                    <li className="header__nav-list-inventory">
+                        <button className={button === "inventoryButton" ? "activeButton" : "inactiveButton"}>
+                            <Link to="/inventory">Inventory</Link>
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 export default Header;
