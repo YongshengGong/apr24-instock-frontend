2d8306ccdb7244703187bf040544c18b6f8f5944
Yongsheng Gong <111623293+YongshengGong@users.noreply.github.com>
2024-06-23 15:55:00 -0400

    Update EditWarehousePage.jsx

--- src/pages/EditWarehousePage/EditWarehousePage.jsx
+++ src/pages/EditWarehousePage/EditWarehousePage.jsx
@@ -1,19 +1,12 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import EditWarehouse from "../../components/EditWarehouse/EditWarehouse";
-import { useParams } from "react-router-dom";
-function EditWarehousePage({ button, passId, warehouses }) {
-  const { warehouseId } = useParams();
-  passId(warehouseId);
-  const selectedWarehouse = warehouses.find(
-    (warehouse) => warehouse.id == warehouseId
-  );
-  // console.log(ID);
-  // console.log(selectedWarehouse);
+function EditWarehousePage({ button, ID, warehouses }) {
+
   return (
     <>
       <Header button={button} />
-      <EditWarehouse selectedWarehouses={selectedWarehouse} />
+      <EditWarehouse warehouses={warehouses} />
       <Footer />
     </>
   );
