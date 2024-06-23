6903b86afd235e002921ecab97d2284537e9fc08
Yongsheng <ygong56@uwo.ca>
2024-06-23 03:35:13 -0400

    Finish connecting frontend to backend API-A24ON-28-createNewInventory

--- src/pages/AddInventoryItemPage/AddInventoryItemPage.jsx
+++ src/pages/AddInventoryItemPage/AddInventoryItemPage.jsx
@@ -1,11 +1,11 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import AddInventoryItem from "../../components/AddInventoryItem/AddInventoryItem";
-function AddInventoryItemPage({button}) {
+function AddInventoryItemPage({button,warehouses}) {
   return (
     <>
       <Header button={button}/>
-        <AddInventoryItem />
+        <AddInventoryItem warehouses={warehouses}/>
       <Footer />
     </>
   );
